5ecd9c2b486d3dd8daa2a53d50fe778d0a2f528e
Gadiel Sosa <sosachanantonio0@gmail.com>
2025-02-13 13:32:45 -0600

    Main text added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -14,12 +14,51 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         //icono izquierda
-        leading: Icon(
-          Icons.menu,
-          color: Colors.grey[800],
+        leading: Padding(
+          padding:
+              const EdgeInsets.only(left: 24.0), //esquina izq espacio de 24
+          child: Icon(
+            Icons.menu,
+            color: Colors.grey[800],
+          ),
         ),
         actions: [
-          Icon(Icons.person),
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: Icon(Icons.person),
+          ),
+        ],
+      ),
+      body: Column(
+        //1.- Texto principal (Text)
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(
+                horizontal: 24.0), // envoltorio edge es borde
+            child: Row(
+              children: [
+                Text(
+                  "I want to ",
+                  style: TextStyle(
+                    fontSize: 32,
+                  ),
+                ),
+                Text(
+                  "Eat",
+                  style: TextStyle(
+                      // tamaño letra
+                      fontSize: 32,
+                      //negritas
+                      fontWeight: FontWeight.bold,
+                      // subrayado
+                      decoration: TextDecoration.underline),
+                ),
+              ],
+            ),
+          )
+          //2.- Pestaña (TabBar)
+          //3.- Contenido de pestañas (TabBarView)
+          //4.- Carrito (Cart)
         ],
       ),
     );
